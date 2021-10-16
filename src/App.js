a3192c36c2820d13ee675ecdeb3e1ebfb786475a
Ingo <ingo-o@yandex.ru>
2021-10-17 01:10:02 +0300

    19. Route, Browser-router

--- src/App.js
+++ src/App.js
@@ -2,18 +2,27 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-import Dialogs from "./components/Dialogs/Dialogs";
+import Messages from "./components/Messages/Messages";
+import {BrowserRouter, Route} from "react-router-dom";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 const App = () => {
     return (
-        <div className='app-wrapper'>
-            <Header/>
-            <Navbar/>
-            <div className='app-wrapper-content'>
-                <Dialogs/>
-                {/*<Profile />*/}
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Route path='/profile' component={Profile}/>
+                    <Route path='/messages' component={Messages}/>
+                    <Route path='/news' component={News}/>
+                    <Route path='/music' component={Music}/>
+                    <Route path='/settings' component={Settings}/>
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
---
+++ src/components/Messages/Messages.jsx
@@ -0,0 +1,11 @@
+import css from "./Messages.module.css";
+
+const Messages = () => {
+    return (
+        <div>
+            Messages
+        </div>
+    );
+};
+
+export default Messages;
