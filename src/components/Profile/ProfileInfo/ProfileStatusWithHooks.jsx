4fbe38ebe6a3affeea1816febb6a5a0b7e107c3c
Ingo <ingo-o@yandex.ru>
2021-12-21 01:35:11 +0300

    85. hook, useEffect

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,11 +1,21 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
+// Локальный state синхронизируется с глобальным один раз в самом начале формирования компоненты.
+// В это время пропсы еще могут отсутствовать (зависит от того что первым придет с сервера, status или profile).
+// Поэтому нужна дополнительная синхронизация локального и глобального статусов через useEffect.
 const ProfileStatusWithHooks = (props) => {
 
     // Хук useState возвращает массив из 2 значений.
-    // Первое это непосредственно наш state (initialState - false). Второе это функция для изменения этого state.
+    // Первое это непосредственно наш state. Второе это функция для изменения этого state.
+    // Через аргумент задается initialState.
     const [editMode, setEditMode] = useState(false);
     const [status, setStatus] = useState(props.status);
+
+    // Хук useEffect запускает колбэк если изменяется что-то из массива условий.
+    // В данном случае он нужен для ситуаций когда status вернулся уже после отрисовки profile.
+    useEffect(() => {
+        setStatus(props.status);
+    }, [props.status]);
 
     const activateEditMode = () => {
         setEditMode(true);
